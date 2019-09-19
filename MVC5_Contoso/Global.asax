cca3f94815a4071972fda3a05ba8ddedaade1d8e
DerekPeacock <derek.peacock@yahoo.com>
2019-09-19 10:38:03 +0100

    Add project files.

---
+++ MVC5_Contoso/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5_Contoso.MvcApplication" Language="C#" %>
---
+++ MVC5_Contoso/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_Contoso.Startup))]
+namespace MVC5_Contoso
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
